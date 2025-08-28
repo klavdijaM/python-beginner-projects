50c128d099c76fd662a53665ca7ec8d9f240ab4a
klavdijaM <klavdija.markovic.dev@gmail.com>
2025-08-28 16:14:22 +0200

    Hangman: - implementing the game logic of losing lives (for each wrong guess 1 life lost) - game runs only until a user wins or loses his lives - for each life lost a hangman figure with additional part of a stickman gets drawn

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,5 +1,62 @@
 import random
 
+stages = [r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 word_list = ["lantern", "harbor", "meadow", "whisper", "metamorphosis", "perpendicularity"]
 
 chosen_word = random.choice(word_list)
@@ -15,25 +72,41 @@ print(placeholder)
 print("Welcome to Hangman! - A game where your spelling skills can save lives (or end them 👀)")
 print("Good Luck!")
 hasWon = False
-guessed_letters = []
+guessed_letters = [] #[l
+lives = 6
 
 # keep guessing until won
-while not hasWon:
+while not hasWon and lives>0:
     guess = input("Guess a letter: ").lower() #l
-    guessed_letters.append(guess) #l
+    guessed_letters.append(guess)
 
     # displaying correctly guessed letters
     display = ""
-    for letter in chosen_word:
+    correct_guess = False
+    for letter in chosen_word: #lantern - la
         if letter in guessed_letters:
-            display += letter
+            display += letter #l
+            if letter == guess: # revise later
+                correct_guess = True
         else:
             display += "_"
     print(display)
 
+    if correct_guess:
+        print("Nice, keep going!")
+    else:
+        # losing lives for wrong guesses
+        if guess not in chosen_word:
+            lives -= 1
+            print(stages[lives])
+            print(f"Oops, wrong guess! You have {lives} lives left")
+
     if "_" not in display:
         hasWon = True
         print("Congratulations! You saved the stickman! 🥳")
+
+if lives == 0 and not hasWon:
+    print("You ran out of lives. RIP stickman 💀")
 
 
 
