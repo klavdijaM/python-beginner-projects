c5ea2283f776ec77024fa26bf3f37cd16dff91fb
klavdijaM <klavdija.markovic.dev@gmail.com>
2025-08-28 15:37:55 +0200

    Hangman: - fixed the game loop - added logic to keep the previously guessed letters in the placeholder when guessing a new letter

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -7,20 +7,34 @@ print(chosen_word)
 
 # displaying a placeholder for all letters in the chosen word
 placeholder = ""
-for underscore in range(len(chosen_word)):
+for letter in range(len(chosen_word)):
     placeholder += "_"
 print(placeholder)
 
+# game start
 print("Welcome to Hangman! - A game where your spelling skills can save lives (or end them 👀)")
 print("Good Luck!")
-guess = input("Guess a letter: ").lower()
+hasWon = False
+guessed_letters = []
 
-# displaying correctly guessed letters
-display = ""
-for letter in chosen_word: # wh
-    if letter == guess:
-        display += letter
-    else:
-        display += "_"
-print(display)
+# keep guessing until won
+while not hasWon:
+    guess = input("Guess a letter: ").lower() #l
+    guessed_letters.append(guess) #l
+
+    # displaying correctly guessed letters
+    display = ""
+    for letter in chosen_word:
+        if letter in guessed_letters:
+            display += letter
+        else:
+            display += "_"
+    print(display)
+
+    if "_" not in display:
+        hasWon = True
+        print("Congratulations! You saved the stickman! 🥳")
+
+
+
 
